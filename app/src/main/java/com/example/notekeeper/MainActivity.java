d27704f3375d2eccff386ffc0407c1e042a41354
Nick Loyd <nickloyd1997@gmail.com>
2020-02-12 08:51:27 -0800

    Add values resources

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -155,6 +155,10 @@ public class MainActivity extends AppCompatActivity
             displayNotes();
         } else if (id == R.id.nav_courses) {
             displayCourses();
+        } else if (id == R.id.nav_share) {
+            handleSelection(R.string.nav_share_message);
+        } else if (id == R.id.nav_send) {
+            handleSelection(R.string.nav_send_message);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -162,9 +166,9 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
 
     }
 }
