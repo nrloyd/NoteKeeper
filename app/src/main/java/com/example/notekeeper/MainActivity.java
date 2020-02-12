b621fe0afabf94fdfd1d9ba88f249422e2d8e284
Nick Loyd <nickloyd1997@gmail.com>
2020-02-12 08:36:39 -0800

    Add values resources

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -101,7 +101,8 @@ public class MainActivity extends AppCompatActivity
     private void initializeDisplayContent() {
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this,
+                getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
